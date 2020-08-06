16093e2b42370bbf27bbca7bd435ed41977c63ad
mbis <marek.biszczak-unilowski@gfieast.com>
2020-08-06 11:18:38 +0200

    Implement very first version of spring security

--- src/main/java/com/biszczak/unilowski/marek/dcsworldtrackerapp/DcsWorldTrackerAppApplication.java
+++ src/main/java/com/biszczak/unilowski/marek/dcsworldtrackerapp/DcsWorldTrackerAppApplication.java
@@ -16,10 +16,8 @@ public class DcsWorldTrackerAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DcsWorldTrackerAppApplication.class, args);
-		//TODO create quick test data loaders
 		//TODO add exceptions and encapsulate all data save/read
 		//TODO Start thinking of implementing spring security into this project
-		//TODO Make also stats calculator for each player etc how many kills, deaths and stuff.
 
 	}
 	@Bean
